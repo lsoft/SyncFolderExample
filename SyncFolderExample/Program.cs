5cae13a6454ce9db120f3d849a37941211d15161
lsoft <lsoft@list.ru>
2016-09-02 12:17:32 +0500

    initial commit

---
+++ SyncFolderExample/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+
+namespace SyncFolderExample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string replica1RootPath = "_R0";
+            const string replica2RootPath = "_R1";
+
+            if (Directory.Exists(replica1RootPath))
+            {
+                Directory.Delete(replica1RootPath, true);
+            }
+            Directory.CreateDirectory(replica1RootPath);
+
+            if (Directory.Exists(replica2RootPath))
+            {
+                Directory.Delete(replica2RootPath, true);
+            }
+            Directory.CreateDirectory(replica2RootPath);
+
+            try
+            {
+                // Set options for the synchronization operation
+                const FileSyncOptions options = 
+                    FileSyncOptions.ExplicitDetectChanges |
+                    FileSyncOptions.RecycleDeletedFiles |
+                    FileSyncOptions.RecyclePreviousFileOnUpdates |
+                    FileSyncOptions.RecycleConflictLoserFiles;
+
+                var filter = new FileSyncScopeFilter();
+                //filter.FileNameExcludes.Add("*.lnk"); // Exclude all *.lnk files
+
+                Guid replica1Guid = Guid.Parse("181517DE-B950-4e62-9582-56F01884288D");
+                Guid replica2Guid = Guid.Parse("86C9D79E-679D-4d33-A051-AA4EEFF17E55");
+
+                using(var provider0 = new FileSyncProvider(replica1Guid, replica1RootPath, filter, options))
+                {
+                    using (var provider1 = new FileSyncProvider(replica2Guid, replica2RootPath, filter, options))
+                    //using (var provider1 = new TestProvider(replica2Guid, Path.Combine(replica2RootPath, "_replica.sdf")))
+                    {
+                        var agent = CreateAgent(
+                            provider0,
+                            provider1
+                            );
+
+                        while (true)
+                        {
+                            provider0.DetectChanges();
+                            provider1.DetectChanges();
+
+                            agent.Synchronize();
+
+                            Thread.Sleep(25);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException from File Synchronization Provider:\n" + e);
+            }
+        }
+
+        public static SyncOrchestrator CreateAgent(
+            FileSyncProvider sourceProvider,
+            FileSyncProvider destinationProvider
+            )
+        {
+            sourceProvider.AppliedChange += OnAppliedChange;
+            sourceProvider.SkippedChange += OnSkippedChange;
+
+            destinationProvider.AppliedChange += OnAppliedChange;
+            destinationProvider.SkippedChange +=  OnSkippedChange;
+
+            var agent = new SyncOrchestrator();
+            agent.LocalProvider = sourceProvider;
+            agent.RemoteProvider = destinationProvider;
+            agent.Direction =
+                SyncDirectionOrder.DownloadAndUpload
+                //SyncDirectionOrder.Upload // Sync source to destination
+                ;
+
+            Console.WriteLine(
+                "Synchronizing changes between {0} <-> {1}",
+                sourceProvider.RootDirectoryPath,
+                destinationProvider.RootDirectoryPath
+                );
+
+            return
+                agent;
+        }
+
+        public static void OnAppliedChange(object sender, AppliedChangeEventArgs args)
+        {
+            switch (args.ChangeType)
+            {
+                case ChangeType.Create:
+                    Console.WriteLine("-- Applied CREATE for file " + args.NewFilePath);
+                    break;
+                case ChangeType.Delete:
+                    Console.WriteLine("-- Applied DELETE for file " + args.OldFilePath);
+                    break;
+                case ChangeType.Update:
+                    Console.WriteLine("-- Applied UPDATE for file " + args.OldFilePath);
+                    break;
+                case ChangeType.Rename:
+                    Console.WriteLine("-- Applied RENAME for file " + args.OldFilePath +
+                                      " as " + args.NewFilePath);
+                    break;
+            }
+        }
+
+        public static void OnSkippedChange(object sender, SkippedChangeEventArgs args)
+        {
+            Console.WriteLine("-- Skipped applying " + args.ChangeType.ToString().ToUpper()
+                              + " for " + (!string.IsNullOrEmpty(args.CurrentFilePath) ?
+                                  args.CurrentFilePath : args.NewFilePath) + " due to error");
+
+            if (args.Exception != null)
+            {
+                Console.WriteLine("   [" + args.Exception.Message + "]");
+            }
+        }
+    }
+}
---
+++ SyncFolderExample/TestProvider.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.MetadataStorage;
+using Microsoft.Synchronization.SimpleProviders;
+
+namespace SyncFolderExample
+{
+    internal class TestProvider : FullEnumerationSimpleSyncProvider, ISimpleSyncProviderConcurrencyConflictResolver, ISimpleSyncProviderConstraintConflictResolver, IDisposable
+    {
+        private readonly string _metadataFilePath;
+        // The version of the provider (not the version of Sync Framework).
+        // Provider version can be used to ensure that two different versions of a provider
+        // can work with each other. For example, if you release version 2 of a provider, but
+        // no fundamtental changes were made to data and metadata storage, handling, or transfer,
+        // the new provider could retain a provider version of 1, which means the two providers
+        // can interoperate.
+        const short PROVIDER_VERSION = 1;
+
+        // The integer value for each column in the item store. These values are used when
+        // creating custom field definitions and identity rules for the ItemMetadataSchema.
+        public const uint CUSTOM_FIELD_ID = 1;
+        public const uint CUSTOM_FIELD_TIMESTAMP = 2;
+
+        private readonly SyncId _replicaId;
+
+        private readonly SyncIdFormatGroup _idFormats;
+        private readonly ItemMetadataSchema _metadataSchema;
+        
+        private volatile bool _disposed;
+
+        private SqlMetadataStore _metadataStore;
+
+        public override SyncIdFormatGroup IdFormats
+        {
+            get
+            {
+                return
+                    _idFormats;
+            }
+        }
+
+        public override ItemMetadataSchema MetadataSchema
+        {
+            get
+            {
+                return
+                    _metadataSchema;
+            }
+        }
+
+        public override short ProviderVersion
+        {
+            get
+            {
+                return
+                    PROVIDER_VERSION;
+            }
+        }
+
+        public TestProvider(
+            Guid replicaId,
+            string metadataFilePath
+            )
+        {
+            _metadataFilePath = metadataFilePath;
+            _replicaId = new SyncId(replicaId);
+
+            //// Set ReplicaIdFormat to use a GUID as an ID, and ItemIdFormat to use a GUID plus
+            //// an 8-byte prefix.
+            //_idFormats = new SyncIdFormatGroup();
+            //_idFormats.ItemIdFormat.IsVariableLength = false;
+            //_idFormats.ItemIdFormat.Length = 24;
+            //_idFormats.ReplicaIdFormat.IsVariableLength = false;
+            //_idFormats.ReplicaIdFormat.Length = 16;
+
+            this._idFormats = new SyncIdFormatGroup();
+            this._idFormats.ChangeUnitIdFormat.IsVariableLength = false;
+            this._idFormats.ChangeUnitIdFormat.Length = (ushort)4;
+            this._idFormats.ItemIdFormat.IsVariableLength = false;
+            this._idFormats.ItemIdFormat.Length = (ushort)24;
+            this._idFormats.ReplicaIdFormat.IsVariableLength = false;
+            this._idFormats.ReplicaIdFormat.Length = (ushort)16;
+
+            CreateMetadataStore(metadataFilePath);
+            _metadataSchema = CreateMetadataSchema();
+        }
+
+        public override void BeginSession()
+        {
+            //nothing to do
+        }
+
+        public override MetadataStore GetMetadataStore(out SyncId replicaId, out CultureInfo culture)
+        {
+            CreateMetadataStore(_metadataFilePath);
+
+            replicaId = _replicaId;
+            culture = CultureInfo.InvariantCulture;
+            
+            return
+                _metadataStore;
+        }
+
+        public override object LoadChangeData(ItemFieldDictionary keyAndExpectedVersion, IEnumerable<SyncId> changeUnitsToLoad, RecoverableErrorReportingContext recoverableErrorReportingContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InsertItem(object itemData, IEnumerable<SyncId> changeUnitsToCreate, RecoverableErrorReportingContext recoverableErrorReportingContext, out ItemFieldDictionary keyAndUpdatedVersion, out bool commitKnowledgeAfterThisItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateItem(object itemData, IEnumerable<SyncId> changeUnitsToUpdate, ItemFieldDictionary keyAndExpectedVersion, RecoverableErrorReportingContext recoverableErrorReportingContext, out ItemFieldDictionary keyAndUpdatedVersion, out bool commitKnowledgeAfterThisItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeleteItem(ItemFieldDictionary keyAndExpectedVersion, RecoverableErrorReportingContext recoverableErrorReportingContext, out bool commitKnowledgeAfterThisItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EndSession()
+        {
+            CloseMetadataStore();
+        }
+
+        public override void EnumerateItems(FullEnumerationContext context)
+        {
+            List<ItemFieldDictionary> items = new List<ItemFieldDictionary>();
+            //foreach (ulong id in _metadataStore.Ids)
+            //{
+            //    items.Add(_store.CreateItemFieldDictionary(id));
+            //}
+            context.ReportItems(items);
+        }
+
+        public void ResolveUpdateUpdateConflict(object itemData, IEnumerable<SyncId> changeUnitsToMerge, IEnumerable<SyncId> changeUnitsToUpdate, ItemFieldDictionary keyAndExpectedVersion, RecoverableErrorReportingContext recoverableErrorReportingContext, out ItemFieldDictionary updatedVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResolveLocalDeleteRemoteUpdateConflict(object itemData, IEnumerable<SyncId> changeUnitsToUpdate, RecoverableErrorReportingContext recoverableErrorReportingContext, out bool itemWasDeletedAsResultOfResolution, out ItemFieldDictionary updatedVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResolveLocalUpdateRemoteDeleteConflict(ItemFieldDictionary keyAndExpectedVersion, RecoverableErrorReportingContext recoverableErrorReportingContext, out bool itemWasDeletedAsResultOfResolution)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MergeConstraintConflict(object itemData, ConflictVersionInformation conflictVersionInformation, IEnumerable<SyncId> changeUnitsToMerge, ItemFieldDictionary localConflictingItem, ItemFieldDictionary keyAndExpectedVersion, RecoverableErrorReportingContext recoverableErrorReportingContext, out ItemFieldDictionary updatedKeyAndVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifyAndInsertRemoteItem(object itemData, IEnumerable<SyncId> changeUnitsToCreate, RecoverableErrorReportingContext recoverableErrorReportingContext, out ItemFieldDictionary updatedKeyAndVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifyAndUpdateRemoteItem(object itemData, IEnumerable<SyncId> changeUnitsToUpdate, ItemFieldDictionary keyAndExpectedVersion, RecoverableErrorReportingContext recoverableErrorReportingContext, out ItemFieldDictionary updatedKeyAndVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifyLocalItem(ItemFieldDictionary keyAndExpectedVersion, RecoverableErrorReportingContext recoverableErrorReportingContext, out ItemFieldDictionary updatedKeyAndVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+
+                CloseMetadataStore();
+            }
+        }
+
+        #region private code
+
+        private void CreateMetadataStore(
+            string metadataFilePath
+            )
+        {
+            if (metadataFilePath == null)
+            {
+                throw new ArgumentNullException("metadataFilePath");
+            }
+
+            SqlMetadataStore result;
+
+            // Create or open the metadata store, initializing it with the ID formats 
+            // that are used to reference items and replicas.
+            if (!File.Exists(metadataFilePath))
+            {
+                result = SqlMetadataStore.CreateStore(metadataFilePath);
+            }
+            else
+            {
+                result = SqlMetadataStore.OpenStore(metadataFilePath);
+            }
+
+            _metadataStore = result;
+        }
+
+        private void CloseMetadataStore()
+        {
+            _metadataStore.Dispose();
+        }
+
+        private ItemMetadataSchema CreateMetadataSchema()
+        {
+            var customFields = new CustomFieldDefinition[2];
+            customFields[0] = new CustomFieldDefinition(CUSTOM_FIELD_ID, typeof (ulong));
+            customFields[1] = new CustomFieldDefinition(CUSTOM_FIELD_TIMESTAMP, typeof (ulong));
+
+            var identityRule = new IdentityRule[1];
+            identityRule[0] = new IdentityRule(new uint[] {CUSTOM_FIELD_ID});
+
+            var result = new ItemMetadataSchema(customFields, identityRule);
+
+            return
+                result;
+        }
+
+        #endregion
+
+    }
+}
